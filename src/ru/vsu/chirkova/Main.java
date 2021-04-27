4d9cbdfb30532db7620181ab3373e431ecb70ff4
seedswithsalt <72308569+seedswithsalt@users.noreply.github.com>
2021-04-27 11:36:07 +0300

    1

--- src/ru/vsu/chirkova/Main.java
+++ src/ru/vsu/chirkova/Main.java
@@ -1,36 +1,35 @@
 package ru.vsu.chirkova;
-import java.util.Scanner;
+
+import java.math.*;
+import java.io.*;
 
 class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Number A: ");
-        long i = in.nextLong();
-        System.out.println("Number B: ");
-        long j = in.nextLong();
-        System.out.print("Result: " + kara(i, j));
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Number A:");
+        BigInteger A = new BigInteger(br.readLine().trim());
+        System.out.println("Number B:");
+        BigInteger B = new BigInteger(br.readLine().trim());
+        System.out.print("Result: ");
+        System.out.print((kara(A, B)).toString());
     }
 
-    public static long kara(long i, long j) {
-        if (i < 10 || j < 10)
-            return i * j;
-        double n = getLength(Math.max(i, j));
-        if (n % 2 == 1)
-            n++;
-        long a = (long) (i / Math.pow(10, (n / 2)));
-        long b = (long) (i % Math.pow(10, (n / 2)));
-        long c = (long) (j / Math.pow(10, (n / 2)));
-        long d = (long) (j % Math.pow(10, (n / 2)));
+    public static BigInteger kara(BigInteger x, BigInteger y) {
+        int N = Math.max(x.bitLength(), y.bitLength());
 
-        long first = kara(a, c);
-        long second = kara(b, d);
-        long third = kara(a + b, c + d);
+        if (N <= 2000) return x.multiply(y);
+        N = (N / 2) + (N % 2);
 
-        return ((long) ((first * Math.pow(10, n)) + ((third - first - second) * Math.pow(10, (n / 2))) + second));
-    }
+        BigInteger b = x.shiftRight(N);
+        BigInteger a = x.subtract(b.shiftLeft(N));
+        BigInteger d = y.shiftRight(N);
+        BigInteger c = y.subtract(d.shiftLeft(N));
 
-    public static int getLength(long a) {
-        String b = Long.toString(a);
-        return b.length();
+        BigInteger ac = kara(a, c);
+        BigInteger bd = kara(b, d);
+
+        BigInteger abcd = kara(a.add(b), c.add(d));
+
+        return ac.add(abcd.subtract(ac).subtract(bd).shiftLeft(N)).add(bd.shiftLeft(2 * N));
     }
 }
